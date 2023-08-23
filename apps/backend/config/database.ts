76135007be1be36716a92e5e9bf5323411150566
Sang Dang <sangdth@gmail.com>
2023-08-23 22:33:35 +0300

    we do not need details anymore

--- apps/backend/config/database.ts
+++ apps/backend/config/database.ts
@@ -4,11 +4,6 @@ export default ({ env }) => {
       client: 'postgres',
       connection: {
         connectionString: env('DATABASE_URL', ''),
-        host: env('PGHOST', 'localhost'),
-        port: env.int('PGPORT', 5432),
-        database: env('PGDATABASE', 'postgres'),
-        user: env('PGUSER', 'postgres'),
-        password: env('PGPASSWORD', 'password'),
         ssl: env.bool(true),
       },
       pool: { min: 0, max: 10 },
